59c3a85144956847f28954e7aec8a3c3bc3d945a
lebegueduweb <patvokouma@gmail.com>
2018-08-27 22:06:01 +0300

    altered many fields on sources files

--- skeleton_recovery/wsgi.py
+++ skeleton_recovery/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for skeleton_recovery project.
+WSGI config for skeleton project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
